dff6c17d7cd0de31722ce3c31f39564712abb8c4
tkysn10281028 <95330554+tkysn10281028@users.noreply.github.com>
2025-05-19 19:28:25 +0900

    first commit

---
+++ src/main/java/com/oysterworld/portfolio/owproject_backend_v2/OwprojectBackendV2Application.java
@@ -0,0 +1,13 @@
+package com.oysterworld.portfolio.owproject_backend_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwprojectBackendV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwprojectBackendV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/oysterworld/portfolio/owproject_backend_v2/OwprojectBackendV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oysterworld.portfolio.owproject_backend_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwprojectBackendV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
